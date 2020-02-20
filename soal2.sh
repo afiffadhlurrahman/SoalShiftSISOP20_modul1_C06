1573b24fcdc2c9b5b77c96fe37ad49f68b1c62ec
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2020-02-20 04:27:48 +0000

    Update soal2.sh

--- soal2.sh
+++ soal2.sh
@@ -1,3 +1,12 @@
 #!bin/bash
 
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1 > /home/afif/sisop/mod/$@.txt
+answer=$@
+p=${#answer}
+a=$(expr "$answer" : "[A-Za-z]*$")
+
+if [ $p -eq $a ]
+then
+	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1 > /home/afif/sisop/mod/$@.txt
+else
+	echo "argument hanya berupa alphabet"
+fi
