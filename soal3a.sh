2007ee94f646893cf05e671364b2e599a7f5cbfa
Afif Fadhlurrahman <49097125+afiffadhlurrahman@users.noreply.github.com>
2020-02-20 16:52:18 +0000

    Update soal3a.sh

--- soal3a.sh
+++ soal3a.sh
@@ -2,7 +2,5 @@
 
 for ((num=1; num<=28; num=num+1))
 do
-  wget -O pdkt_kusuma_$num "https://loremflickr.com/320/240/cat"
-  log=wget.log
-  date >> $log
+  wget -a wget.log -O pdkt_kusuma_$num "https://loremflickr.com/320/240/cat"
 done
